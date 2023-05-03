770f269445a90d321ec03a3a67510cdfceba999a
soniabergamini <sounia96@outlook.it>
2023-05-03 16:34:07 +0200

    perf(js): avoid double numbers inside array

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,16 +25,18 @@ const randomNumbers = [];
 // Create Random Numbers
 do {
    let singleNumber = getRandomNumber(1, 100);
-    randomNumbers.push(singleNumber);
-
+    if (randomNumbers.indexOf(singleNumber) === -1) randomNumbers.push(singleNumber); 
+    // indexOf() is a method that returns -1 if the value is not found inside Array.
 } while (randomNumbers.length < 5);
-
 console.log("Array created", randomNumbers);
 
 // Add Random Numbers on Web Page
-mainText.innerText = `Remember these numbers: ${randomNumbers.join(" - ")}`;
+mainText.innerText = `Can you remember these numbers in thirty seconds: ${randomNumbers.join(" - ")} ?`;
 
 // Hide Numbers from page after 30 seconds
 setTimeout(() => mainText.style.display = "none", 5 * 1000); // After debug, change timeout with 30sec
+
+// Ask user numbers after 31 seconds 
+
 
 /****************** EVENTS ******************/

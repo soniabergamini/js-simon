bfc922a3381d8d9e274d451906e555384cc47df9
soniabergamini <sounia96@outlook.it>
2023-05-03 16:20:32 +0200

    feat(main): add random numbers on page

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+console.log("ciao");
+
+/****************** FUNCTIONS ******************/
+
+// Create HTML Elements
+function createElement(tag, id, classes, content) {
+    const element = document.createElement(tag);
+    element.setAttribute("id", id);
+    element.setAttribute("class", classes);
+    element.innerText = (content);
+    return element;
+}
+
+// Create Random Numbers
+function getRandomNumber(min, max) {
+    let number = Math.floor(Math.random() * (max - min + 1) + min);
+    return number;
+}
+
+/****************** VARIABLES ******************/
+const mainTitle = document.getElementById('title');
+const mainText = document.getElementById('numbers');
+const randomNumbers = [];
+
+/****************** INIT ******************/
+
+// Create Random Numbers
+do {
+   let singleNumber = getRandomNumber(1, 100);
+    randomNumbers.push(singleNumber);
+
+} while (randomNumbers.length < 5);
+
+console.log("Array created", randomNumbers);
+
+// Add Random Numbers on Web Page
+mainText.innerText = `Remember these numbers: ${randomNumbers.join(" - ")}`;
+
+
+/****************** EVENTS ******************/
